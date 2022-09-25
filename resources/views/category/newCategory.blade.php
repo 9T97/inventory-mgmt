ebe91b3fd8deea974aeba40135798a7085a71c7d
9T97 <win59813@gmail.com>
2022-09-25 12:04:53 +0630

    refactor[category]: Add error directive for category

--- resources/views/category/newCategory.blade.php
+++ resources/views/category/newCategory.blade.php
@@ -10,11 +10,21 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <form action="{{route('category.store')}}" method="post">
+                        @csrf
                         <div class="mb-6 w-1/2 mx-auto">
                             <label for="category-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Category Name</label>
-                            <input type="text" id="category_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="categoryName" placeholder="Items" required>
+                            <input type="text" id="category_name" class="@error('name') is-invalid @enderror bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="name" required>
                         </div>
-                
+                        @error('name')
+                        <div class="mb-6 w-1/2 mx-auto">
+                            <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+                                {{$message}}
+                            </div>
+                        @enderror
+                        </div>
+                        <div class="mb-6 w-1/2 mx-auto">
+                        <button type="submit" class="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create</button>
+                        </div>                        
                     </form>
                 </div>
             </div>
